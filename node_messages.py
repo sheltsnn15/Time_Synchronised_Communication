28bcc9855d6362caf4420bd2ecd2dd126b0763d4
Shelton Ngwenya <shelt15.nn@gmail.com>
2022-11-10 14:42:06 +0000

    Discovery period implementation (Not Fully Completed)

---
+++ node_messages.py
@@ -0,0 +1,11 @@
+# @author Shelton Ngwenya, R00203947
+
+class NodeMessages:
+    HELLO = "HELLO"
+    BS_HELLO = "BS-HELLO"
+    DEV_HELLO = "DEV-HELLO"
+    SCHED = "SCHED"
+    DATA = "DATA"
+    DATA_PERIOD = "Data period starting"
+    NO_SLOT = "Didn't get a slot :("
+    DISCOVERY_START = "Discovery starts"
